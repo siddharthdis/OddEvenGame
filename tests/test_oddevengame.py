85114e54d95ca283a1c1d0f9e2260890c651d244
siddharthdis <siddharthdis3432@gmail.com>
2023-09-23 17:36:19 +0530

    Odd Even Game

---
+++ tests/test_oddevengame.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `OddEvenGame` package."""
+
+
+import unittest
+
+from oddevengame import oddevengame
+
+
+class TestOddevengame(unittest.TestCase):
+    """Tests for `oddevengame` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
